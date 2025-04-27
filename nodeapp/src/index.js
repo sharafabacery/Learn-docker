71c83db3208bdbe9247aaec7c60aa4ef1ab285bc
Sharafeldin Ehab <sharafeldin.ehab@codezone-eg.com>
2025-04-27 15:53:53 +0300

    postgres

--- nodeapp/src/index.js
+++ nodeapp/src/index.js
@@ -1,6 +1,22 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import { createClient } from 'redis'
+import {Pool, Client } from 'pg'
+const USER_NAME='root'
+const PASSWORD='example'
+const DB_PORT=5432
+const DB_HOST ="postgres"
+//const DB_URI=`mongodb://${USER_NAME}:${PASSWORD}@${DB_HOST}:${DB_PORT}`
+
+const pool = new Pool({
+  user: USER_NAME,
+  password: PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT
+})
+await pool.connect()
+ 
+console.log(await pool.query('SELECT NOW()'))
 
 const PORT=process.env.PORT|| 4000
 const app=express()
@@ -8,25 +24,25 @@ const app=express()
 const REDIS_PORT=6379
 const REDIS_HOST="redis"
 
-const client = await createClient({
+const clientRedis = await createClient({
     url: `redis://${REDIS_HOST}:${REDIS_PORT}`
 })
   .on('error', err => console.log('Redis Client Error', err))
   .connect();
 
-const USER_NAME='root'
-const PASSWORD='example'
-const DB_PORT=27017
-const DB_HOST ="mongo"
-const DB_URI=`mongodb://${USER_NAME}:${PASSWORD}@${DB_HOST}:${DB_PORT}`
-mongoose.connect(DB_URI).then(
-()=>{
-    console.log("connected")
-}
+//const USER_NAME='root'
+//const PASSWORD='example'
+//const DB_PORT=27017
+//const DB_HOST ="mongo"
+//const DB_URI=`mongodb://${USER_NAME}:${PASSWORD}@${DB_HOST}:${DB_PORT}`
+//mongoose.connect(DB_URI).then(
+//()=>{
+//    console.log("connected")
+//}
 
-).catch((err)=>{
-console.log(err)
-});
+//).catch((err)=>{
+//console.log(err)
+//});
 
 app.use(express.json());
 app.get('/api/test', (req, res) => {
