6e066d9d516fd069c35b44269d40d6e2412aec70
Sharafeldin Ehab <sharafeldin.ehab@codezone-eg.com>
2025-04-27 15:08:52 +0300

    nginx tutorial

--- nodeapp/src/index.js
+++ nodeapp/src/index.js
@@ -49,6 +49,26 @@ app.get('/api/test', (req, res) => {
     // Send the response back to the client
     res.json(responseMessage);
 });
+app.get('/', (req, res) => {
+    // Print the request body
+    console.log('Request Body:', req.body);
+
+    // Print query parameters
+    console.log('Query Parameters:', req.query);
+
+    // Prepare the response
+    const responseMessage = {
+        message: 'GET request received!',
+        receivedBody: req.body,  // Include the body in the response for visibility
+        queryParameters: req.query  // Include query parameters in the response
+    };
+
+    // Print the response to console
+    console.log('Response:', responseMessage);
+
+    // Send the response back to the client
+    res.json(responseMessage);
+});
 app.listen(PORT,()=>{
     console.log(PORT)
 })
